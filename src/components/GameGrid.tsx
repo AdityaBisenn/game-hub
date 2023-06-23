eaeb7f2562fc2532ff2fb60b3b028bb76f24547e
Aditya Bisen <adityabisenn@gmail.com>
2023-06-23 19:58:44 +0530

    display platform icons

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -8,7 +8,7 @@ const GameGrid = () => {
   return (
     <>
       {error && <p>{error}</p>}
-      <SimpleGrid columns={{sm:1,md:2,lg:3,xl:5}} spacing={10} padding={'10px'}>
+      <SimpleGrid columns={{sm:1,md:2,lg:3}} spacing={5} padding={'10px'}>
         {games.map((game) => (
             <GameCard key={game.id} game={game} />
         ))}
