28fc3be11f2fe433c5fc68135d891291954177c5
Aditya Bisen <adityabisenn@gmail.com>
2023-06-24 03:00:37 +0530

    changes in image file

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,7 +1,6 @@
-import noImage from "../assets/noimage.webp";
 
 const getCroppedImageUrl = (url : string) => {
-    if(!url) return noImage;
+    if(!url) return "";
   const index = url.indexOf("media/")+6;
   return url.slice(0,index) + "crop/600/400/" + url.slice(index);
 }
