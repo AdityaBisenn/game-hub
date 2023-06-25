c8704b09225e0596f4c29f6f7b9fb381953d9d1c
Aditya Bisen <adityabisenn@gmail.com>
2023-06-25 18:08:51 +0530

    api key changed

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: "https://api.rawg.io/api",
     params: {
-        key: "d9c993f7f421484a861e942d7ab1493f"
+        key: "ddd32a58cad540d2ad39a9d3aef48758"
     }
 })
